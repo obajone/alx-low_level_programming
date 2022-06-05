866420746f543d6b0f1557418804b567fb80704f
Eddington Obaro Jonathan <obajone@gmail.com>
2022-06-05 14:51:38 -0700

    My executable files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,26 +9,26 @@
  */
 int main(void)
 {
-	int n;
-	int lastDig;
+        int n;
+        int lastDig;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	lastDig = n % 10;
+        lastDig = n % 10;
 
-	if (lastDig > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
-	}
-	if (lastDig == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDig);
-	}
-	if (lastDig < 6 && lastDig != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
-	}
-
-	return (0);
+        if (lastDig > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
+        }
+        if (lastDig == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n", n, lastDig);
+        }
+        if (lastDig < 6 && lastDig != 0)
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
+        }
+	putchar("\n")
+        return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,15 @@
 int main(void)
 
 {
-char i;
+	char i;
 
-for (i = 'a'; i <= 'z'; i++)
-{
-putchar(i);
-}
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 
 }
